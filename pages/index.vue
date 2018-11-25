80a675e52164a3851145d17f0706d6dbe748c3c2
Latence132 <alexandre.chaumet@gadz.org>
2018-11-25 22:01:50 +0100

    vue-videobg

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
   <full-page ref="fullpage" :options="options">
+    <!-- class section is for the fullpage-vue -->
+    <video-bg class="section" :sources="[videoBg]" v-lazy:background-image="imageURL2" playsinline muted loop autoplay>
+      <!-- If you want to add content here, a slot is waiting! -->
+    </video-bg>
     <div class="section" v-lazy:background-image="imageURL">
+      <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
       <button class="next" @click="$refs.fullpage.api.moveSectionDown()" style="background-color: red;">Next</button>
     </div>
     <div class="section" v-lazy:background-image="imageURL1">
@@ -17,12 +22,18 @@
 
 
 <script>
+import VideoBg from "vue-videobg/src/VideoBackground.vue";
+
 export default {
+  components: {
+    VideoBg
+  },
   data() {
     return {
       imageURL: 'aerial-aerial-view-architecture-977pxh.jpg',
       imageURL1: 'black-wallpaper-night-sky-7477_977pxh.jpg',
-      imageURL2: 'mer_ecran1.jp2'
+      imageURL2: 'mer_ecran1.jp2',
+      videoBg: '/bg-1.mp4'
     }
   }
 }
