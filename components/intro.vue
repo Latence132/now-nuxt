d4dcfc27996bc366e6cc2e3d568942a99f471054
Latence132 <alexandre.chaumet@gadz.org>
2018-12-10 22:46:22 +0100

    intro debut

---
+++ components/intro.vue
@@ -0,0 +1,56 @@
+<template>
+<v-layout justify-center)>
+  <v-flex class=" text-xs-center xs12 black--text opening">
+    <h2 class="opening fill-height ">
+      <span>coucou</span>
+      <span>c'est</span>
+      <span>nous</span>
+      <span>:)</span>
+      <span></span>
+    </h2>
+  </v-flex>
+</v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      introText: [ 'Bienvenue', 'sur la page de', 'Alexandre Chaumet', 'Développeur web' ]
+    }
+  },
+
+}
+</script>
+
+<style lang="css" >
+.opening span {
+  animation:
+    animateOpen 5s ease-in-out infinite, flash .1s ease-in-out
+  }
+
+
+
+.opening span:nth-child(4n){
+  animation-delay: 3s;
+}
+.opening span:nth-child(4n-1) {
+  animation-delay: 2s;
+}
+.opening span:nth-child(4n-2){
+  animation-delay: 1s;
+}
+.opening span:nth-child(4n-3){
+  animation-delay: 0s;
+}
+
+@keyframes animateOpen{
+  from { opacity:1;}
+  to { opacity:0;}
+  }
+
+@keyframes flash {
+  from { text-shadow: 0 0 32px white;}
+  to { text-shadow: 0 0 0 white;}
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 <div>
   <full-page ref="fullpage" :options="options">
     <!-- class section is for the fullpage-vue -->
-    <video-bg class="section" :sources="[videoBg]" v-lazy:background-image="imageURL2" playsinline muted loop autoplay>
-      <!-- If you want to add content here, a slot is waiting! -->
+    <video-bg class="section" :sources="[videoBg]" v-lazy:background-image="imageURL2" autoplay muted loop>
+      <intro></intro>
     </video-bg>
     <div class="section" v-lazy:background-image="imageURL">
       <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
@@ -23,17 +23,21 @@
 
 <script>
 import VideoBg from "vue-videobg/src/VideoBackground.vue";
-
+import intro from "~/components/intro.vue";
 export default {
   components: {
-    VideoBg
+    VideoBg,
+    intro
   },
   data() {
     return {
       imageURL: 'aerial-aerial-view-architecture-977pxh.jpg',
       imageURL1: 'black-wallpaper-night-sky-7477_977pxh.jpg',
       imageURL2: 'mer_ecran1.jp2',
-      videoBg: '/bg-1.mp4'
+      videoBg: 'bg-1.mp4',
+      options: {
+        licenceKey: 'OPEN-SOURCE-GPLV3-LICENSE'
+      }
     }
   }
 }
