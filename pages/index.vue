3ba1bca99c9843f0d9777e6e65efea5e7ba33b83
Unknown <alexandre.chaumet@gadz.org>
2019-02-24 20:50:43 +0100

    fullpage: added background-size:100%

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,24 @@
 <template>
 <div>
-  <full-page ref="fullpage" :options="options">
+  <full-page id="idFullPage" ref="fullpage" :options="options">
     <!-- class section is for the fullpage-vue -->
-    <video-bg class="section" :sources="[videoBg]" v-lazy:background-image="imageURL2" autoplay muted loop>
-      <intro style="padding-top: 20%" />
-    </video-bg>
-    <div class="section" v-lazy:background-image="imageURL">
+    <div class="section" v-lazy:background-image.idFullPage="imageURL2" :style="styleBg">
+      <video-bg :sources="[videoBg]" autoplay muted loop>
+        <intro style="padding-top: 20%" />
+      </video-bg>
+    </div>
+    
+    <div class="section" v-lazy:background-image="imageURL"  :style="styleBg">
       <neige></neige>
       <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
       <button class="next" @click="$refs.fullpage.api.moveSectionDown()" style="background-color: red;">Next</button>
     </div>
-    <div class="section" v-lazy:background-image="imageURL1">
-      <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
-      <button class="next" @click="$refs.fullpage.api.moveSectionDown()" style="background-color: red;">Next</button>
+    <div class="section"  v-lazy:background-image.idFullPage="imageURL1" :style="styleBg">
+        <inspire></inspire>
+        <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
+        <button class="next" @click="$refs.fullpage.api.moveSectionDown()" style="background-color: red;">Next</button>
     </div>
-    <div class="section" v-lazy:background-image="imageURL2">
+    <div class="section" v-lazy:background-image.idFullPage="imageURL2" :style="styleBg">
       <button class="prev" @click="$refs.fullpage.api.moveSectionUp()" style="background-color: red;">Prev</button>
     </div>
   </full-page>
@@ -26,14 +30,19 @@
 import VideoBg from "vue-videobg/src/VideoBackground.vue";
 import intro from "~/components/intro.vue";
 import neige from "~/components/neige.vue";
+import inspire from "~/pages/inspire.vue"
 export default {
   components: {
     VideoBg,
     intro,
-    neige
+    neige,
+    inspire
   },
   data() {
     return {
+      styleBg:{
+        "background-size": '100%'
+      },
       imageURL: 'aerial-aerial-view-architecture-977pxh.jpg',
       imageURL1: 'black-wallpaper-night-sky-7477_977pxh.jpg',
       imageURL2: 'mer_ecran1.jp2',
@@ -45,3 +54,9 @@ export default {
   }
 }
 </script>
+<style lang="css">
+  img[lazy=loaded] {
+      background-size: 100%;
+    }
+
+</style>
